e45e786737cff7e79a3e30968ce1757e2a51c2df
Chris Oden <coden@its.jnj.com>
2020-05-11 08:50:00 -0400

    Add low level test for contextMenuForm()

--- packages/bodiless-core/__tests__/contextMenuForm.test.tsx
+++ packages/bodiless-core/__tests__/contextMenuForm.test.tsx
@@ -3,10 +3,57 @@
  * Integration tests for context menu forms.
  */
 import React, { useState } from 'react';
-import { mount } from 'enzyme';
-import { ContextMenuForm, FormProps, FormBodyRenderer } from '../src/contextMenuForm';
+import { mount, shallow } from 'enzyme';
+import contextMenuForm, { ContextMenuForm, FormProps, FormBodyRenderer } from '../src/contextMenuForm';
 
-describe('ContextMenuForm', () => {
+describe('contextMenuForm', () => {
+  it('Applies options as props to the innter ContextMenuForm', () => {
+    const render = jest.fn();
+    const submitValues = jest.fn();
+    const initialValues = {
+      foo: 'bar',
+    };
+
+    const Form = contextMenuForm({ submitValues, initialValues })(render);
+    // @ts-ignore We're not providng all necessary props here.
+    const wrapper = shallow(<Form />);
+    wrapper.prop('children')();
+    expect(render).toHaveBeenCalled();
+    wrapper.prop('submitValues')();
+    expect(submitValues).toHaveBeenCalled();
+    expect(wrapper.prop('initialValues').foo).toBe('bar');
+  });
+
+  it('Allows options to be overridden', () => {
+    const options = {
+      children: jest.fn(),
+      submitValues: jest.fn(),
+      initialValues: {
+        foo: 'bar',
+      },
+    };
+    const props = {
+      children: jest.fn(),
+      submitValues: jest.fn(),
+      initialValues: {
+        foo: 'baz',
+      },
+    };
+
+    const Form = contextMenuForm(options)(options.children);
+    // @ts-ignore We're not providng all necessary props here.
+    const wrapper = shallow(<Form {...props} />);
+    wrapper.prop('children')();
+    expect(props.children).toHaveBeenCalled();
+    expect(options.children).not.toHaveBeenCalled();
+    wrapper.prop('submitValues')();
+    expect(props.submitValues).toHaveBeenCalled();
+    expect(options.submitValues).not.toHaveBeenCalled();
+    expect(wrapper.prop('initialValues').foo).toBe('baz');
+  });
+});
+
+describe('ContextMenuForm (High Level)', () => {
   it('Allows state from an enclosing component to be used in the form and submit handler', () => {
     const submit = jest.fn();
     const close = jest.fn();
