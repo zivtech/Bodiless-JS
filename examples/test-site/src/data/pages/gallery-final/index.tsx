d1457d15f173cccf3eebd61347c3c2dcf15ac156
hvanyo <58534559+hvanyo@users.noreply.github.com>
2020-07-20 14:35:59 -0600

    fix(test-site): Change gallery-final header to landscape image (#452)
    * Update README.md
    
    * Revert "Update README.md"
    
    This reverts commit 6e092146b372883462ba7525ac1eb6602834dfdb.
    
    * Change gallery-final to landscape

--- examples/test-site/src/data/pages/gallery-final/index.tsx
+++ examples/test-site/src/data/pages/gallery-final/index.tsx
@@ -17,11 +17,10 @@ import { graphql } from 'gatsby';
 import { Page, PageProps } from '@bodiless/gatsby-theme-bodiless';
 import { flow } from 'lodash';
 import {
-  H1, Img, A, addClasses,
+  H1, addClasses,
 } from '@bodiless/fclasses';
-import { asEditable, asBodilessLink } from '@bodiless/components';
-import { asBodilessImage } from '@bodiless/components-ui';
-
+import { asEditable } from '@bodiless/components';
+import { LandscapeLinkableImage } from '../../../components/Image';
 import Layout from '../../../components/Layout';
 import withSimpleEditor from './withSimpleEditor';
 import Gallery, { GalleryTile } from './Gallery';
@@ -32,15 +31,12 @@ const PrimaryHeader = flow(
   asEditable('title', 'Title'),
 )(H1);
 
-const Link = asBodilessLink('hero')(A);
-const Image = asBodilessImage('image')(Img);
 const Body = withSimpleEditor('body', 'Body')(Fragment);
 
 export default (props: PageProps) => (
   <Page {...props}>
     <Layout>
-      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-      <Link><Image /></Link>
+      <LandscapeLinkableImage />
       <PrimaryHeader />
       <Body />
       <Gallery nodeKey="gallery">
