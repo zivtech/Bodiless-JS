14e75948f2856908f24781b64469df6c8810e7c6
Andrei Beliayeu <53858490+beliayeu@users.noreply.github.com>
2020-07-29 16:01:25 +0300

    feat(documentation): API Doc for Site builder (#474)

--- packages/bodiless-documentation/src/blDocsBuild.ts
+++ packages/bodiless-documentation/src/blDocsBuild.ts
@@ -25,6 +25,7 @@ import {
 import { writeSideBars, writeNavBar } from './createBar';
 import { Tree } from './type';
 import readSettings from './readSettings';
+import buildApiDoc, { updateNavigation as apiDocUpdateNavigation } from './blApiDocsBuild';
 
 const buildSubTree = async (toc: any, namespace: string) => {
   // We start by using locateFiles and withTreeFromFile to build up an array of TreeHO and
@@ -73,15 +74,18 @@ const blDocsBuild = async () => {
   } catch (error) {
     console.warn('Error writing symlinks', error);
   }
-  console.log('Writing sidebars');
+
+  // Let api doc builder to update navigation links in runtime
+  const navigationPaths = apiDocUpdateNavigation(docPath, paths);
+
   try {
-    await writeSideBars(docPath, paths);
+    await writeSideBars(docPath, navigationPaths);
   } catch (error) {
     console.warn('Error writing sidebars', error);
   }
   console.log('Writing navbar');
   try {
-    await writeNavBar(docPath, paths);
+    await writeNavBar(docPath, navigationPaths);
   } catch (error) {
     console.warn('Error writing navbar', error);
   }
@@ -89,7 +93,13 @@ const blDocsBuild = async () => {
   try {
     await writeResources(docPath, copier);
   } catch (error) {
-    console.warn('Error writing navbar', error);
+    console.warn('Error writing resources', error);
+  }
+  console.log('Building API docs');
+  try {
+    await buildApiDoc({ targetDocPath: docPath, copier });
+  } catch (error) {
+    console.warn('Error building API docs', error);
   }
   console.log('Done');
 };
