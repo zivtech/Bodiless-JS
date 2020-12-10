eca21e1872a30e879eb5a57e4a07a91d39e2fad9
Chris Oden <coden@its.jnj.com>
2020-12-10 08:04:24 -0500

    Fix some linting issues.

--- packages/bodiless-components/__tests__/Editable.test.tsx
+++ packages/bodiless-components/__tests__/Editable.test.tsx
@@ -1,36 +1,24 @@
 import React from 'react';
 import { flow } from 'lodash';
 
-import { mount, render } from 'enzyme';
+import { render } from 'enzyme';
 import { withMockNode } from './helpers/MockContentNode';
 import { asEditable } from '../src';
 
 describe('asEditable', () => {
-  it('Invokes a processor when not in edit mode', () => {
-    const data = {
-      text: 'Now is the time',
-    };
-    const sanitizer = (text: string) => text.replace(/i/g, '*');
-    const useOverrides = () => ({ sanitizer });
-    const Test = flow(
-      asEditable('foo', 'Foo', useOverrides),
-      withMockNode(data),
-    )('apan');
-    const wrapper = render(<Test />);
-    expect(wrapper.text()).toBe('Now *s the t*me');
-  });
-
-  it('Invokes a processor when not in edit mode', () => {
-    const data = {
-      text: 'Now is the time',
-    };
-    const sanitizer = (text: string) => text.replace(/i/g, '*');
-    const useOverrides = () => ({ sanitizer });
-    const Test = flow(
-      asEditable('foo', 'Foo', useOverrides),
-      withMockNode(data),
-    )('apan');
-    const wrapper = render(<Test />);
-    expect(wrapper.text()).toBe('Now *s the t*me');
+  describe('When not editable', () => {
+    it('Invokes a sanitizer', () => {
+      const data = {
+        text: 'Now is the time',
+      };
+      const sanitizer = (text: string) => text.replace(/i/g, '*');
+      const useOverrides = () => ({ sanitizer });
+      const Test = flow(
+        asEditable('foo', 'Foo', useOverrides),
+        withMockNode(data),
+      )('apan');
+      const wrapper = render(<Test />);
+      expect(wrapper.text()).toBe('Now *s the t*me');
+    });
   });
 });
